8fb5e34726bf5fac93b0d808b6671870c5476164
Priskin Zsuzsanna <priskin.zsuzsanna@gmail.com>
2023-04-16 13:02:06 +0200

    font

--- src/pages/Pokemons.js
+++ src/pages/Pokemons.js
@@ -1,7 +1,5 @@
-//import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-//import axios from 'axios'
 import PokemonCard from '../components/PokemonCard'
 import Logo from '../assets/logo.png';
 import styled from 'styled-components';
